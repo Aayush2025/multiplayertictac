2072298c859df7ebadd756ab9d5c40222934c401
Aayush <aayushnarnoli2907@gmail.com>
2024-10-17 00:45:23 +0530

    Server Updated

--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 const server = require('http').createServer()
 const io = require('socket.io')(server, {
     cors: {
-        origin: "tictactoe-pink-mu.vercel.app",
+        origin: "https://tictactoe-pink-mu.vercel.app",
         methods: ["GET", "POST"]
     }
 });
