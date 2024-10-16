ac0c92e40ab7dde0ae44fd947d2f77d300b41408
Aayush <aayushnarnoli2907@gmail.com>
2024-10-17 01:01:07 +0530

    Last Try

--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 const server = require('http').createServer()
 const io = require('socket.io')(server, {
     cors: {
-        origin: "https://tictactoe-8yk6vji8u-aayush-narnolis-projects.vercel.app/",
+        origin: "https://tictactoe-pink-mu.vercel.app/",
         methods: ["GET", "POST"]
     }
 });
